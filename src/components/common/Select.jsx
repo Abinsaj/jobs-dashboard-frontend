f7734963c56027feb77d1fea1b8c4aba56b77ab0
Abinsaj <148989575+Abinsaj@users.noreply.github.com>
2025-12-19 19:49:50 +0530

    resolve select dropdown overlap

--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -1,13 +1,19 @@
 import { ChevronDown } from 'lucide-react'
 import React from 'react'
 
-const Select = ({value, onChange, options, placeholder}) => {
+const Select = ({ value, onChange, options, placeholder }) => {
   return (
     <div className="relative w-full sm:w-auto">
       <select
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="appearance-none bg-[#1a1a1a] rounded-lg  md:px-4 py-2.5 pr-10 text-gray-500 text-xs border-0 outline-none ring-0 focus:border-0 focus:outline-none focus:ring-0 ">
+        className={` appearance-none bg-[#1a1a1a] rounded-lg px-4 py-2.5 pr-12 min-w-[160px] text-xs border-0 outline-none focus:ring-0
+    ${value === ""
+            ? "text-gray-500"
+            : "text-gray-200"
+          }
+  `}
+      >
         <option className='text-sm' value="">{placeholder}</option>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
@@ -15,7 +21,10 @@ const Select = ({value, onChange, options, placeholder}) => {
           </option>
         ))}
       </select>
-      <ChevronDown className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" size={18} />
+      <ChevronDown
+        className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none"
+        size={16}
+      />
     </div>
   )
 }
